92008d4705d4a67da52c030342ba063a6f308e98
Armandomateus41 <armandomateus41@gmail.com>
2022-08-12 14:08:09 -0300

    actualização

--- src/app/heores/heores.component.ts
+++ src/app/heores/heores.component.ts
@@ -1,19 +1,22 @@
+import { HEROES } from './../mock-heroes';
 import { hero } from './../hero.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'app-heores',
   templateUrl: './heores.component.html',
   styleUrls: ['./heores.component.scss']
 })
-export class HeoresComponent implements OnInit {
-  hero:  hero =   {
+export class HeoresComponent{
+hero:  hero =   {
 id: 1,
 name:'wolverine'
   };
-  constructor() { }
+ 
+heroes =HEROES;
+selectedHero?: hero;
 
-  ngOnInit(): void {
+onSelect(hero: hero): void {
+this.selectedHero = hero;
   }
-
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { hero } from './hero.model';
+
+export const HEROES: hero [] =  [
+  { id: 1, name: 'wolverine'},
+  { id: 2, name: 'Homem aranha'},
+  { id: 3, name: 'Batmam'},
+  { id: 4, name: 'Mulher maravilha'},
+  { id: 5, name: 'Pantera Negra'},
+  { id: 6, name: 'Capita Marvel'},
+  { id: 7, name: 'Hulk'},
+  { id: 8, name: 'Capitao America'},
+
+];
